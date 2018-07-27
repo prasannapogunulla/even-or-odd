b6f8cac3fbd35437aa8cd8f2ccedfe7fefe8780e
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-07-27 10:42:28 +0530

    Create Demo1.java

---
+++ Demo1.java
@@ -0,0 +1,14 @@
+public class Demo1
+{
+public static void main(Sring args[])
+{
+int number;
+if(number%2==0)
+{
+System.out.println("number+"is even number);
+}
+else
+{
+System.out.println("number+"is odd number);
+}
+}
